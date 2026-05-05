5b6aa0b7f59b2bc00c1fbb879a0f3b99bd793ea8
sadra <sadramohtork@gmail.com>
2026-05-05 21:30:04 +0200

    adding a better main menu system and more presentable

--- core/src/main/java/com/test/game/states/MenuState.java
+++ core/src/main/java/com/test/game/states/MenuState.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.test.game.JumpyBirb;
@@ -11,11 +13,18 @@ import com.test.game.JumpyBirb;
 public class MenuState extends State {
     private Texture background;
     private Sound backgroundMusic = Gdx.audio.newSound(Gdx.files.internal("background_music.mp3")); //music source: https://freesound.org/people/rebrie18/
+    private BitmapFont font;
+    private GlyphLayout startLayout;
+    private GlyphLayout titleLayout;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
         background = new Texture("bg.png");
         backgroundMusic.play();
+        font = new BitmapFont(Gdx.files.internal("winter_font.fnt"));
+        font.getData().setScale(1f);
+        titleLayout = new GlyphLayout(font, "Jumpy Bird");
+        startLayout = new GlyphLayout(font, "Starta Spelet");
     }
 
     @Override
@@ -34,12 +43,18 @@ public class MenuState extends State {
     public void render(SpriteBatch batch, ShapeRenderer renderer) { //draw starts bottom left-hand corner, continues right and upwards
         batch.begin();
         batch.draw(background, 0, 0, JumpyBirb.WIDTH, JumpyBirb.HEIGHT);
+        font.draw(batch, titleLayout, (JumpyBirb.WIDTH - titleLayout.width) / 2, JumpyBirb.HEIGHT * 0.95f);
+        font.draw(batch, startLayout,
+            (JumpyBirb.WIDTH - startLayout.width) / 2,
+            JumpyBirb.HEIGHT * 0.5f);
         batch.end();
+
     }
 
     @Override
     public void dispose() {
         background.dispose();
+        font.dispose();
         System.out.println("MenuState disposed");
     }
 }
